b93ead47d818a3ce7ab5e2a58a95c4fc99d7bd2a
Denis Andrei Guimpu Stefan <7032@iesluisbraille.es>
2022-03-10 12:15:26 +0100

    line 6 if added

--- HyperPar.java
+++ HyperPar.java
@@ -1,16 +1,20 @@
 public class HyperPar {
 
 
-        public static boolean hyperpar(int num){
-            int resto;
-            while(num!=0) {
-            resto = num%10;
-            num = num/10;
-            if(resto%2 != 0)
-            {
-                return false;
-            }
+    public static boolean hyperpar(int num) {
+        int resto;
+        if (num >= 0) {
+            while (num != 0) {
+                resto = num % 10;
+                num = num / 10;
+                if (resto % 2 != 0) {
+                    return false;
+                }
             }
             return true;
         }
+        else {
+            return false;
+        }
+    }
 }
